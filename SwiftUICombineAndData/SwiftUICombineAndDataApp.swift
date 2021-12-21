23ac776aea71191831141f42beea0f5754ee33ad
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-12-21 19:08:17 +0500

    Initial Commit

---
+++ SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineAndDataApp.swift
+//  SwiftUICombineAndData
+//
+//  Created by Arkasha Zuev on 21.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
